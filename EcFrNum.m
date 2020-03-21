47f24716cbe0a9874025a553efcb99f214607263
Marius Dirvareanu <dirvareanumarius@live.com>
2020-03-21 20:57:18 +0200

    Ecuatie Fredholm matrice rezolvata

--- EcFrNum.m
+++ EcFrNum.m
@@ -8,6 +8,7 @@ function EcFrNum(a,b,lambda,I)
     h=(b-a)/I;
     x=a:h:b;
     function k=k(x,t)
+%         k=x*t/25;
         if (x>0 && x<t)
             k=sin(x)*cos(t);
         else
@@ -17,13 +18,13 @@ function EcFrNum(a,b,lambda,I)
 
     function f=f(x)
         f=cos(x);
+%         f=pi*x/25*(cos(lambda*pi)-1)+sin(lambda*x);
     end
     for i=1:I+1
         F(i,1)=f(x(i));
     end
     figure(1)
     for i=1:I+1
-        val(i)=0;
         for j=1:I+1
             if j==1
                 A(i,j)=h/2*lambda*k(x(i),x(1));
@@ -34,9 +35,8 @@ function EcFrNum(a,b,lambda,I)
                     A(i,j)=h*lambda*k(x(i),x(j));
                 end
             end
-            val(i)=val(i)+A(i,j)*U(j,1);
+            U(i,1)=A(i,j)*U(j,1)+F(i,1);
         end
-        U(i,1)=val(i)+F(i,1);
     end
     plot(x,U,'r');
 end
--- test.m
+++ test.m
@@ -1,20 +1,4 @@
 clc; clear all; close all;
-
-function f=f(x)
-    f=pi*x/25*(cos(alpha*pi)-1)+sin(alpha*x);
-end
-
-function k=k(x,y)
-    k=x*y/25;
-end
-
-function phi=phi(t)
-    phi=f(t)+int((k(t,s)*phi(s)),s,0,pi)
-end
-
-
-I=40;
-
-for i=1:I+1 
-   phi(i)
-end
+EcFrNum(0,pi,3,100)
+% hold on;
+% EcFr(0,pi,3,100,500)
